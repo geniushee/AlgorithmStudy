d47b029007bb42fabffa92b3c1986ea11c74462d
geniushee <h1116jeon@gmail.com>
2025-06-21 14:26:08 +0900

    적록색약 문제 완료

--- src/main/java/org/ll/backjun/RedGreen.java
+++ src/main/java/org/ll/backjun/RedGreen.java
@@ -1,8 +1,7 @@
 package org.ll.backjun;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -34,9 +33,163 @@ import java.util.Queue;
  * 4 3
  */
 public class RedGreen {
-    public static void main(String[] arg){
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // init
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        int[][] image = new int[N][N];
+        boolean[][] red = new boolean[N][N];
+        boolean[][] green = new boolean[N][N];
+        boolean[][] blue = new boolean[N][N];
+        Queue<int[]> queue = new LinkedList<>();
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+        int normal = 0;
+        int blind = 0;
 
+        // set image, red = 1, green = 2, blue = 3
+        for (int i = 0; i < N; i++) {
+            line = br.readLine();
+            int j = 0;
+            for (String s : line.split("")) {
+                int e = 0;
+                switch (s) {
+                    case "R" -> e = 1;
+                    case "G" -> e = 2;
+                    case "B" -> e = 3;
+                }
+                image[i][j++] = e;
+            }
+        }
+
+        // find section
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                boolean flag = false;
+                if (!red[i][j] && !green[i][j] && !blue[i][j]) {
+                    switch (image[i][j]) {
+                        case 1 -> {
+                            if (red[i][j]) {
+                                continue;
+                            }
+                            red[i][j] = true;
+                        }
+                        case 2 -> {
+                            if (green[i][j]) {
+                                continue;
+                            }
+                            green[i][j] = true;
+                        }
+                        case 3 -> {
+                            if (blue[i][j]) {
+                                continue;
+                            }
+                            blue[i][j] = true;
+                        }
+                    }
+                    queue.offer(new int[]{i, j, image[i][j]});
+                    flag = true;
+                }
+                while (!queue.isEmpty()) {
+                    int[] now = queue.poll();
+                    for (int k = 0; k < 4; k++) {
+                        int nx = now[0] + dx[k];
+                        int ny = now[1] + dy[k];
+                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                            continue;
+                        }
+                        if (now[2] == image[nx][ny]) {
+                            switch (now[2]) {
+                                case 1 -> {
+                                    if (red[nx][ny]) {
+                                        continue;
+                                    }
+                                    red[nx][ny] = true;
+                                }
+                                case 2 -> {
+                                    if (green[nx][ny]) {
+                                        continue;
+                                    }
+                                    green[nx][ny] = true;
+                                }
+                                case 3 -> {
+                                    if (blue[nx][ny]) {
+                                        continue;
+                                    }
+                                    blue[nx][ny] = true;
+                                }
+                            }
+                            queue.offer(new int[]{nx, ny, image[nx][ny]});
+                        }
+                    }
+                }
+                if (flag) {
+                    normal++;
+                }
+            }
+        }
+        bw.write(normal+"\n");
+
+        //init
+        red = new boolean[N][N];
+        blue = new boolean[N][N];
+        // find section
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                boolean flag = false;
+                if (!red[i][j] && !blue[i][j]) {
+                    switch (image[i][j]) {
+                        case 1, 2 -> {
+                            if (red[i][j]) {
+                                continue;
+                            }
+                            red[i][j] = true;
+                        }
+                        case 3 -> {
+                            if (blue[i][j]) {
+                                continue;
+                            }
+                            blue[i][j] = true;
+                        }
+                    }
+                    queue.offer(new int[]{i, j, image[i][j]});
+                    flag = true;
+                }
+                while (!queue.isEmpty()) {
+                    int[] now = queue.poll();
+                    for (int k = 0; k < 4; k++) {
+                        int nx = now[0] + dx[k];
+                        int ny = now[1] + dy[k];
+                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                            continue;
+                        }
+                        if (now[2] == 3 && image[nx][ny] == 3) {
+                            if (blue[nx][ny]) {
+                                continue;
+                            }
+                            blue[nx][ny] = true;
+                            queue.offer(new int[]{nx, ny, image[nx][ny]});
+                        } else if(now[2] != 3 && (image[nx][ny] == 1 || image[nx][ny] == 2)) {
+                            if (red[nx][ny]) {
+                                continue;
+                            }
+                            red[nx][ny] = true;
+                            queue.offer(new int[]{nx, ny, image[nx][ny]});
+                        }
+                    }
+                }
+                if (flag) {
+                    blind++;
+                }
+            }
+        }
+        bw.write(blind+"\n");
+        bw.flush();
+        bw.close();
     }
+
     public static void run(String arg) throws IOException {
         BufferedReader br = new BufferedReader(new StringReader(arg));
         // init
@@ -47,57 +200,152 @@ public class RedGreen {
         boolean[][] green = new boolean[N][N];
         boolean[][] blue = new boolean[N][N];
         Queue<int[]> queue = new LinkedList<>();
-        int[] dx = new int[]{-1,1,0,0};
-        int[] dy = new int[]{0,0,-1,1};
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
         int normal = 0;
         int blind = 0;
 
         // set image, red = 1, green = 2, blue = 3
-        for(int i = 0; i<N;i++){
+        for (int i = 0; i < N; i++) {
             line = br.readLine();
             int j = 0;
-            for(String s : line.split("")){
+            for (String s : line.split("")) {
                 int e = 0;
-                switch(s){
-                    case "R"-> e = 1;
+                switch (s) {
+                    case "R" -> e = 1;
                     case "G" -> e = 2;
                     case "B" -> e = 3;
                 }
                 image[i][j++] = e;
             }
         }
+        System.out.println(Arrays.deepToString(image));
 
         // find section
-        for(int i = 0; i<N;i++){
-            for(int j=0; j<N;j++){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 boolean flag = false;
-                if(!red[i][j] || !green[i][j] || !blue[i][j]){
-                    queue.offer(new int[]{i,j,image[i][j]});
+                if (!red[i][j] && !green[i][j] && !blue[i][j]) {
+                    switch (image[i][j]) {
+                        case 1 -> {
+                            if (red[i][j]) {
+                                continue;
+                            }
+                            red[i][j] = true;
+                        }
+                        case 2 -> {
+                            if (green[i][j]) {
+                                continue;
+                            }
+                            green[i][j] = true;
+                        }
+                        case 3 -> {
+                            if (blue[i][j]) {
+                                continue;
+                            }
+                            blue[i][j] = true;
+                        }
+                    }
+                    queue.offer(new int[]{i, j, image[i][j]});
                     flag = true;
                 }
-                while(!queue.isEmpty()){
+                while (!queue.isEmpty()) {
                     int[] now = queue.poll();
-                    for(int k = 0; k < 4; k++){
+                    System.out.printf("현재위치 : %d, %d, %d\n", now[0], now[1], now[2]);
+                    for (int k = 0; k < 4; k++) {
                         int nx = now[0] + dx[k];
                         int ny = now[1] + dy[k];
-                        if(nx < 0 || nx >= N || ny < 0 || ny >= N){
+                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
                             continue;
                         }
-                        if(now[2] == image[nx][ny]){
-                            switch(now[2]){
-                                case 1 -> red[nx][ny] = true;
-                                case 2 -> green[nx][ny] = true;
-                                case 3 -> blue[nx][ny] = true;
+                        if (now[2] == image[nx][ny]) {
+                            switch (now[2]) {
+                                case 1 -> {
+                                    if (red[nx][ny]) {
+                                        continue;
+                                    }
+                                    red[nx][ny] = true;
+                                }
+                                case 2 -> {
+                                    if (green[nx][ny]) {
+                                        continue;
+                                    }
+                                    green[nx][ny] = true;
+                                }
+                                case 3 -> {
+                                    if (blue[nx][ny]) {
+                                        continue;
+                                    }
+                                    blue[nx][ny] = true;
+                                }
                             }
-                            queue.offer(new int[]{nx, ny,image[nx][ny]});
+                            queue.offer(new int[]{nx, ny, image[nx][ny]});
                         }
                     }
                 }
-                if(flag) normal++;
+                if (flag) {
+                    System.out.println("한 구역 종료");
+                    normal++;
+                }
             }
         }
-        System.out.println("일반: "+normal);
+        System.out.println("일반: " + normal);
 
-
+        //init
+        red = new boolean[N][N];
+        blue = new boolean[N][N];
+        // find section
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                boolean flag = false;
+                if (!red[i][j] && !blue[i][j]) {
+                    switch (image[i][j]) {
+                        case 1, 2 -> {
+                            if (red[i][j]) {
+                                continue;
+                            }
+                            red[i][j] = true;
+                        }
+                        case 3 -> {
+                            if (blue[i][j]) {
+                                continue;
+                            }
+                            blue[i][j] = true;
+                        }
+                    }
+                    queue.offer(new int[]{i, j, image[i][j]});
+                    flag = true;
+                }
+                while (!queue.isEmpty()) {
+                    int[] now = queue.poll();
+                    System.out.printf("현재위치 : %d, %d, %d\n", now[0], now[1], now[2]);
+                    for (int k = 0; k < 4; k++) {
+                        int nx = now[0] + dx[k];
+                        int ny = now[1] + dy[k];
+                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                            continue;
+                        }
+                        if (now[2] == 3 && image[nx][ny] == 3) {
+                            if (blue[nx][ny]) {
+                                continue;
+                            }
+                            blue[nx][ny] = true;
+                            queue.offer(new int[]{nx, ny, image[nx][ny]});
+                        } else if(now[2] != 3 && (image[nx][ny] == 1 || image[nx][ny] == 2)) {
+                            if (red[nx][ny]) {
+                                continue;
+                            }
+                            red[nx][ny] = true;
+                            queue.offer(new int[]{nx, ny, image[nx][ny]});
+                        }
+                    }
+                }
+                if (flag) {
+                    System.out.println("한 구역 종료");
+                    blind++;
+                }
+            }
+        }
+        System.out.println("색약: " + blind);
     }
 }
