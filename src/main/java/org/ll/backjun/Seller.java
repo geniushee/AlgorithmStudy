8ffa21e8737594d0bb9b23c71416be01c64462a3
geniushee <h1116jeon@gmail.com>
2025-07-12 14:02:25 +0900

    tsp문제 완료, 매 시행마다 dp를 초기화 한다는 점. 역으로 누적하면서 올라오는 방식이 참신함

--- src/main/java/org/ll/backjun/Seller.java
+++ src/main/java/org/ll/backjun/Seller.java
@@ -3,6 +3,7 @@ package org.ll.backjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
 
 /**
  * 문제<br>
@@ -33,8 +34,8 @@ public class Seller {
 
     public static void run(String arg) throws IOException {
         BufferedReader br = new BufferedReader(new StringReader(arg));
+        // init
         N = Integer.parseInt(br.readLine());
-        dp = new int[1<<N][N];
         min = Integer.MAX_VALUE;
         V = new int[N][N];
         for(int i = 0; i < N; i++){
@@ -44,33 +45,45 @@ public class Seller {
             }
         }
 
+
         for(int i = 0; i<N;i++){
-            min = Math.min(min, tsp(1 << i,i));
+            dp = new int[1<<N][N];
+            for(int[] row : dp) Arrays.fill(row, -1);
+
+            int m = tsp(1 << i,i,i);
+            min = Math.min(min, m);
         }
         System.out.println(min);
     }
 
-    private static int tsp(int visited, int current){
+    /**
+    *최소를 구하는 과정이 최종 목적지에서 출발하여 결과를 기록하며 출발선까지 기록하는 것.
+     **/
+    private static int tsp(int visited, int current, int start){
+        // 최종 목적지에서 드는 비용을 반환
         if(visited == (1<<N) -1){
-            return dp[visited][current];
+            if(V[current][start] == 0){
+                return Integer.MAX_VALUE;
+            }
+            return V[current][start];
         }
+
+        if(dp[visited][current] != -1) return dp[visited][current];
+
         int m = Integer.MAX_VALUE;
 
         for(int i = 0; i<N;i++){
             // 갈 수 있고 방문을 안했으면
-            if(V[current][i] != 0 && (visited & 1<<i) > 0){
+            if(V[current][i] != 0 && (visited & 1<<i) == 0){
                 // 다음 방문
                 int nextVisit = visited | (1<<i);
+                int nextV = tsp(nextVisit, i, start);
                 // 기록
-                if(dp[nextVisit][i] == 0){
-                    dp[nextVisit][i] = dp[visited][current] + V[current][i];
-                    m = Math.min(m, tsp(nextVisit, i));
-                }else if(dp[nextVisit][i] > dp[visited][current] + V[current][i]){
-                    dp[nextVisit][i] = dp[visited][current] + V[current][i];
-                    m = Math.min(m, tsp(nextVisit, i));
+                if(nextV != Integer.MAX_VALUE){
+                    m = Math.min(m, V[current][i] + nextV);
                 }
             }
         }
-        return m;
+        return dp[visited][current] = m;
     }
 }
